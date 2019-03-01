df1b3ba1c6985a697ef3fc87192eb5fa374850e0
Steven Chavarria <steven.chavarria4@gmail.com>
2019-03-01 17:48:22 -0600

    Add files via upload

--- DimVehiculo.sql
+++ DimVehiculo.sql
@@ -1,14 +1,14 @@
-use TareaAnalisis;
+use TareaAnalisis
 
 IF OBJECT_ID('DimVehiculo', 'U') IS NOT NULL
 	DROP TABLE DimVehiculo;
 CREATE TABLE DimVehiculo(
 Id_Vehiculo int,
-Tipo_Vehiculo nvarchar(20),
-Fab_Vehiculo nvarchar(20),
-Pais_Fab_Vehiculo nvarchar(20),
-Abv_Pais_Fab_Vehiculo nvarchar(3),
-Region_Fab_Vehiculo nvarchar(10),
-Continente_Fab_Vehiculo nvarchar(10),
+Tipo_Vehiculo nvarchar(50),
+Fab_Vehiculo nvarchar(50),
+Pais_Fab_Vehiculo nvarchar(50),
+Abv_Pais_Fab_Vehiculo nvarchar(50),
+Region_Fab_Vehiculo nvarchar(50),
+Continente_Fab_Vehiculo nvarchar(50),
 Estrato_Ano_Fab int
-)
+);
