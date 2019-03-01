8c05fc7b6a17b354c7645698ae98e181701ae302
Steven Chavarria <steven.chavarria4@gmail.com>
2019-03-01 17:52:07 -0600

    Add files via upload

--- DimParqueoInsert.sql
+++ DimParqueoInsert.sql
@@ -1,9 +1,10 @@
+
 insert 
 into [TareaAnalisis]..DimParqueo
 
 select p.IDParqueo, d.Descripcion as 'Nombre Distrito', pr.Descripcion as 'Nombre Provincia',ca.Descripcion as 'Nombre Canton'
 from [ExamenAnalisis]..Parqueo p 
-inner join Distrito d on p.ConsecutivoDistrito = d.ConsecutivoDistrito 
-inner join Provincia pr on d.IDProvincia=pr.IDProvincia
-inner join Canton ca on ca.IDCanton = d.IDCanton and ca.IDProvincia = d.IDProvincia
+inner join [ExamenAnalisis]..Distrito d on p.ConsecutivoDistrito = d.ConsecutivoDistrito 
+inner join [ExamenAnalisis]..Provincia pr on d.IDProvincia=pr.IDProvincia
+inner join [ExamenAnalisis]..Canton ca on ca.IDCanton = d.IDCanton and ca.IDProvincia = d.IDProvincia
 order by 1 asc;
--- Insert DimVehiculo.sql
+++ Insert DimVehiculo.sql
@@ -2,10 +2,10 @@ insert
 into [TareaAnalisis]..DimVehiculo
 
 select v.IDVehiculo, tv.Descripcion as 'Nombre tipo vehiculo', f.NombreFabricante, p.NombrePais,p.AbreviaturaPais, rc.Descripcion as 'Nombre Region', c.Descripcion as 'Nombre Continente', v.Ano
-from [TareaAnalisis]..Vehiculo v
-inner join TipoVehiculo tv on tv.IDTipoVehiculo = v.IDTipoVehiculo
-inner join Fabricante f on f.IDFabricante = v.IDFabricante
-inner join Pais p  on p.IDPais = f.IDPais
-inner join RegionContinente rc on rc.IDRegionContinente = p.IDRegionContinente
-inner join Continente c on c.IDContinente = rc.IDContinente
+from [ExamenAnalisis]..Vehiculo v
+inner join [ExamenAnalisis]..TipoVehiculo tv on tv.IDTipoVehiculo = v.IDTipoVehiculo
+inner join [ExamenAnalisis]..Fabricante f on f.IDFabricante = v.IDFabricante
+inner join [ExamenAnalisis]..Pais p  on p.IDPais = f.IDPais
+inner join [ExamenAnalisis]..RegionContinente rc on rc.IDRegionContinente = p.IDRegionContinente
+inner join [ExamenAnalisis]..Continente c on c.IDContinente = rc.IDContinente
 order by 1 asc;
