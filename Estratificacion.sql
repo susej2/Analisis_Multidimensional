f87d7a4505f5a622e93eabc12ba94da30aa685fa
susej2 <35945119+susej2@users.noreply.github.com>
2019-03-06 20:35:39 -0600

    Add files via upload

--- Estratificacion.sql
+++ Estratificacion.sql
@@ -1,14 +1,14 @@
 USE TareaAnalisis;
 IF OBJECT_ID('DimEstratificacion', 'U') IS NOT NULL
 	DROP TABLE DimEstratificacion;
-CREATE TABLE DimEstratificacion (
-	ID int PRIMARY KEY,
-	CodigoEstrato VARCHAR (10),
-	Descripcion VARCHAR (50),
-	Ano_Fab  INT,
-	Tiempo_Est  INT,
-	Hora_Inicio_Est  time,
-	Hora_Final_Est  time,
-	Ganancia_Obt money
-)
-;
+
+CREATE TABLE [dbo].[DimEstratificacion](
+	[ID] [int] PRIMARY KEY,
+	[CodigoEstrato] [nvarchar](20) NULL,
+	[Descripcion] [varchar](50) NULL,
+	[Valor_Minimo] [nvarchar](50) NULL,
+	[Valor_Maximo] [nvarchar](50) NULL,
+	);
+GO
+
+
